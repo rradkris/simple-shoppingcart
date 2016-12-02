c6a0f5df01b3161ad4efa9267c96ace4b2447b6c
Ravi Radhakrishnan <ravi.radhakrishnan@anatwine.com>
2016-12-02 12:43:32 +0000

    Simple offer shopping cart implementation with Unit tests

--- src/main/scala/com/simple/shopping/cart/service/Checkout.scala
+++ src/main/scala/com/simple/shopping/cart/service/Checkout.scala
@@ -12,4 +12,24 @@ case class Checkout() {
       case (product, numberOfProductInCart) => numberOfProductInCart * product.price
     }.sum
   }
+
+  def checkoutWithOffers(cart: ShoppingCart):Double = {
+    cart.products.view.map {
+      case (product, numberOfProductInCart)
+        if (product.name == "apple") => costOfApplesInCart(product.price, numberOfProductInCart)
+      case (product, numberOfProductInCart)
+        if (product.name == "orange") => costOfOrangesInCart(product.price, numberOfProductInCart)
+    }.sum
+  }
+
+  def costOfApplesInCart(price: Double, numberOfApples: Long):Double = {
+    ((numberOfApples / 2) + (numberOfApples % 2)) * price
+  }
+
+  def costOfOrangesInCart(price: Double, numberOfOranges: Long) = {
+    numberOfOranges match {
+      case numberOfOranges if (numberOfOranges % 3 == 0) => ((numberOfOranges / 3) * 2) * price
+      case numberOfOranges => ((numberOfOranges % 3) * price) + ((numberOfOranges / 3) * 2) * price
+    }
+  }
 }
--- src/test/scala/com/simple/shopping/cart/service/CheckoutSpec.scala
+++ src/test/scala/com/simple/shopping/cart/service/CheckoutSpec.scala
@@ -25,5 +25,41 @@ class CheckoutSpec extends Specification {
       Checkout().checkout(shoppingCart) mustEqual 2.05
     }
   }
+
+
+  "total cost must be 0.60 when two apples in the cart" in {
+    val shoppingCart = ShoppingCart()
+      .addToCart(Apple)
+      .addToCart(Apple)
+    Checkout().checkoutWithOffers(shoppingCart) mustEqual 0.6
+  }
+
+  "total cost must be 1.20 when three apples in the cart" in {
+    val shoppingCart = ShoppingCart()
+      .addToCart(Apple)
+      .addToCart(Apple)
+      .addToCart(Apple)
+    Checkout().checkoutWithOffers(shoppingCart) mustEqual 1.2
+  }
+
+  "total cost must be 1.00 when six oranges found in the cart" in {
+    val shoppingCart = ShoppingCart()
+      .addToCart(Orange)
+      .addToCart(Orange)
+      .addToCart(Orange)
+      .addToCart(Orange)
+      .addToCart(Orange)
+      .addToCart(Orange)
+    Checkout().checkoutWithOffers(shoppingCart) mustEqual 1.0
+  }
+
+  "total cost must be 0.75 when four oranges found in the cart" in {
+    val shoppingCart = ShoppingCart()
+      .addToCart(Orange)
+      .addToCart(Orange)
+      .addToCart(Orange)
+      .addToCart(Orange)
+    Checkout().checkoutWithOffers(shoppingCart) mustEqual 0.75
+  }
 }
 
